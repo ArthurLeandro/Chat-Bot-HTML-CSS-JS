bd232c2514509daeb710b50814a4ee09fb876d4d
Arthur Leandro <arthur.leandro11@gmail.com>
2020-06-20 18:50:45 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,12 @@ var serialNumber = null;
 var knowResponses={
 // Resposta do individuo : resposta do bot
 // "Não":"<iframe width='360' height='275' src='https://www.youtube.com/embed/TQIBx6rgntc'</iframe>",
-"Gostaria de saber algo":"<a href='' class='anchor-in-chat'>Algo</a>",
-"Por favor":"Vamos treinar",
-"Obrigado":"Eu que agradeço pela disposição",
+// "Gostaria de saber algo":"<a href='' class='anchor-in-chat'>Algo</a>",
 "1":" Muito bem! Sinal de que você está antenado quanto às normas da empresa. Você sabe qual a importância desse treinamento?",
 "2":"Poxa, que pena! Nós falamos tanto dele com todos. Você sabe qual a importância desse treinamento? (Sim ou Não)",
-"Sim":"Que ótimo! Você está no caminho certo. Eu entrarei em contato com meus superiores para informar sua situação.",
-"Não":"Não se preocupe, eu vou te ajudar.O treinamento de BPF visa estabelecer normas e medidas que todos devemos ter e tomar em relação ao nosso ambiente aqui de trabalho.",
-"Obrigado":"Eu que agradeço, pela sua paciência e cordialidade."
+"SIM":"Que ótimo! Você está no caminho certo. Eu entrarei em contato com meus superiores para informar sua situação.",
+"NÃO":"Não se preocupe, eu vou te ajudar.O treinamento de BPF visa estabelecer normas e medidas que todos devemos ter e tomar em relação ao nosso ambiente aqui de trabalho.",
+"OBRIGADO":"Eu que agradeço, pela sua paciência e cordialidade."
 };
 var nameWasTaken = false;
 var serialNumberWasTaken = true;
@@ -33,8 +31,8 @@ function talk(){
     if(userResponse == serialNumber){ //conseguiu pegar o valor da matricula
       setTimeout(function(){CreateTheChatView("speach-view","Agora vamos lá. Você sabe o que é o treinamento de Boas Práticas de Fabricação? Me informe o número correspondente a sua resposta. (1 para sim ,2 para não)",null);},1000);
     }
-    else if(userResponse in knowResponses){
-      setTimeout(function(){ CreateTheChatView("speach-view",knowResponses[userResponse],null);},1000);
+    else if(userResponse.toUpperCase() in knowResponses){
+      setTimeout(function(){ CreateTheChatView("speach-view",knowResponses[userResponse.toUpperCase()],null);},1000);
     }
     else if(userResponse == name){
       serialNumberWasTaken = false;
@@ -52,10 +50,12 @@ function talk(){
     let pResponse = document.createElement("p");
     if(_name == null || _name == undefined){
       nameh6.innerHTML = "Vilminha";  
+      pResponse.innerHTML = _userResponse;
     }else{
       nameh6.innerHTML = name;
+      pResponse.innerHTML = _userResponse;
     }
-    pResponse.innerHTML = _userResponse;
+    
     divBox.classList.add(_class);
     nameh6.classList.add("speach-name-view");    
     pResponse.classList.add("speach-content-view");
